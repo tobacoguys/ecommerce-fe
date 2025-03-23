1083c1b9872810083f88388a7211209984bf7a14
tobacoguys <dungchelsea1723@gmail.com>
2025-03-23 23:27:42 +0700

    Add product modal and country list management

--- src/app/context/ThemeProvider.jsx
+++ src/app/context/ThemeProvider.jsx
@@ -1,5 +1,6 @@
 import { MyContext } from "./ThemeContext";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const ThemeProvider = ({ children }) => {
     const [isLogin, setIsLogin] = useState(false);
@@ -7,6 +8,8 @@ const ThemeProvider = ({ children }) => {
     const [isOpenProductModal, setIsOpenProductModal] = useState(false);
     const [ cartData, setCartData ] = useState([]);
     const [categoryData, setCategoryData] = useState([]);
+    const [productData, setProductData] = useState([]);
+    const [countryList, setCountryList] = useState([]);
 
     const getCartData = () => {
         const user = JSON.parse(localStorage.getItem("user"));
@@ -16,6 +19,23 @@ const ThemeProvider = ({ children }) => {
               });          
         }
     };
+
+    const openProductDetailsModal = (id, status) => {
+        fetchDataFromApi(`/api/product/${id}`).then((res) => {
+            setProductData(res);
+            setIsOpenProductModal(status);
+        });
+    }
+
+    const getCountry = async (url) => {
+        const responsive = await axios.get(url).then((res) => {
+          setCountryList(res.data.data)
+        })
+      }
+
+    useEffect(() => {
+        getCountry("https://countriesnow.space/api/v0.1/countries/");
+    }, []);
 
     useEffect(() => {
         getCartData();
