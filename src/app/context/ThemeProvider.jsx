46fa077e4f107930bcea7483f4a8869833299830
tobacoguys <dungchelsea1723@gmail.com>
2025-03-25 00:07:55 +0700

    fix useEffect handle window resize

--- src/app/context/ThemeProvider.jsx
+++ src/app/context/ThemeProvider.jsx
@@ -76,13 +76,17 @@ const ThemeProvider = ({ children }) => {
         setAlertBox({ open: false });
     };
       
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-      
     useEffect(() => {
-        const handleResize = () => setWindowWidth(window.innerWidth);
+        const handleResize = () => {
+          setWindowWidth(window.innerWidth);
+        };
+    
         window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      }, []);
 
     const addToCart = (data) => {
         if (isLogin) {
